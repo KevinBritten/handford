3441f94c4bb732bf8ccefbb67d9cf5c4896f3a82
Kevin Britten <kevinjbritten@gmail.com>
2023-09-14 18:32:58 -0400

    add assets from webflow to repo

--- scripts/main.js
+++ scripts/main.js
@@ -40,10 +40,10 @@ const processItemShow = (i) => {
   processItems.forEach((item, index) => {
     if (!item.classList.contains(`_${i + 1}`)) {
       item.style.display = "none";
-      processNumbers[index].classList.remove("--0-opacity");
+      processNumbers[index].classList.remove("--0-opacity-desktop");
     } else {
       item.style.display = "block";
-      processNumbers[index].classList.add("--0-opacity");
+      processNumbers[index].classList.add("--0-opacity-desktop");
     }
   });
 };
